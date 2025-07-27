2402c198c4ddfbde962072243055bb18d7260985
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2025-07-27 19:34:04 +0800

    [web] Add logo and description to the registration form
    - Added logo to the registration form.
    - Included a brief description to guide users through the registration process.

--- core-cloud-web/src/app/views/register-view.tsx
+++ core-cloud-web/src/app/views/register-view.tsx
@@ -19,6 +19,8 @@
 
 'use client'
 
+import ImageView from '@/ui-elements/views/image-view'
+import SFSymbolView from '@/ui-elements/views/sf-symbol-view'
 import ReactDOM from 'react-dom'
 
 export default function RegisterView({
@@ -52,7 +54,118 @@ export default function RegisterView({
           >
             <div className="h-full flex flex-col">
               <div className="flex-1 min-h-0 overflow-y-auto">
-                <div className="h-256">placeholder</div>
+                <div
+                  className={
+                    'max-w-135 w-4/5 mt-7.5 mx-auto flex flex-col items-center'
+                  }
+                >
+                  <div className="h-12.75 mb-1.25 flex items-center">
+                    {
+                      process.env.NEXT_PUBLIC_AUTHENTICATOR_ICON_URLS
+                        ? (
+                          <ImageView
+                            urls={
+                              process.env.NEXT_PUBLIC_AUTHENTICATOR_ICON_URLS
+                                .split(',')
+                            }
+                            className="size-6"
+                          />
+                        )
+                        : (
+                          <SFSymbolView
+                            systemName="icloud.fill"
+                            className="w-6 fill-sk-body-text-color"
+                          />
+                        )
+                    }
+                  </div>
+                  <h1
+                    className={
+                      'text-[28px] font-semibold text-body-text-color ' +
+                        'leading-8 mb-3.75'
+                    }
+                  >
+                    {'Create Your '}
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: process.env.NEXT_PUBLIC_TITLE ?? ''
+                      }}
+                    />
+                    {' Account'}
+                  </h1>
+                  <p className="text-base leading-6.25 text-body-text-color">
+                    {'One '}
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: process.env.NEXT_PUBLIC_TITLE ?? ''
+                      }}
+                    />
+                    {' Account is all you need to access all '}
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: process.env.NEXT_PUBLIC_TITLE ?? ''
+                      }}
+                    />
+                    {' services.'}
+                  </p>
+                  <p
+                    className={
+                      'text-base leading-6.25 text-body-text-color mb-2.5'
+                    }
+                  >
+                    {'Already have a '}
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: process.env.NEXT_PUBLIC_TITLE ?? ''
+                      }}
+                    />
+                    {' Account? '}
+                    <button
+                      className={
+                        'pr-[18.7px] text-sk-body-link-color cursor-pointer ' +
+                          'text-base relative'
+                      }
+                    >
+                      {'Sign In'}
+                      <svg
+                        viewBox="0 0 268.0201416015625 158.116943359375"
+                        version="1.1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={
+                          'absolute h-[20.046875px] top-[3.4px] ' +
+                            '-right-[8.5px] fill-sk-body-link-color'
+                        }
+                      >
+                        <g
+                          transform={
+                            'matrix(1 0 0 1 85.49510009765618 ' +
+                              '114.2884521484375)'
+                          }
+                        >
+                          <path
+                            d={
+                              'M84.5703-17.334L84.5215-66.4551C84.5215-' +
+                                '69.2383 82.7148-71.1914 ' +
+                                '79.7852-71.1914L30.6641-71.1914C27.9297-' +
+                                '71.1914 26.0742-69.0918 26.0742-' +
+                                '66.748C26.0742-64.4043 28.1738-62.4023 ' +
+                                '30.4688-62.4023L47.4609-62.4023L71.2891-' +
+                                '63.1836L62.207-55.2246L13.8184-' +
+                                '6.73828C12.9395-5.85938 12.4512-4.73633 ' +
+                                '12.4512-3.66211C12.4512-1.31836 14.5508 ' +
+                                '0.878906 16.9922 0.878906C18.1152 0.878906 ' +
+                                '19.1895 0.488281 20.0684-0.439453L68.5547-' +
+                                '48.877L76.6113-58.0078L75.7324-' +
+                                '35.2051L75.7324-17.1387C75.7324-14.8438 ' +
+                                '77.7344-12.6953 80.127-12.6953C82.4707-' +
+                                '12.6953 84.5703-14.6973 84.5703-17.334Z'
+                            }
+                          />
+                        </g>
+                      </svg>
+                    </button>
+                  </p>
+                </div>
               </div>
 
               <div
