e943bc431376c80053e7f9600f483ee251c87892
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2025-09-21 08:27:05 +0800

    [web] Make image non-selectable

--- core-cloud-web/sources/ui/src/views/async-image.tsx
+++ core-cloud-web/sources/ui/src/views/async-image.tsx
@@ -56,7 +56,7 @@ export default function AsyncImage({
         src={urls[urls.length - 1]}
         className={
           `${widthClassName} ${heightClassName} ${marginClassName} ` +
-            `${borderClassName} ` + (
+            `${borderClassName} select-none ` + (
               contentMode === ContentMode.fit ? "object-contain" : ""
             )
         }
