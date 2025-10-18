579bbf7b21b7392afbb9a6b5d99e83decb5da35d
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2025-10-18 19:37:05 +0800

    [wasm] Remove unused symbols

--- core-cloud-wasm/src/sha512.c
+++ core-cloud-wasm/src/sha512.c
@@ -75,26 +75,11 @@
 #include <string.h>
 #include <stdint.h>
 
-/*
- * General byte order swapping functions.
- */
-#define  bswap16(x)  __bswap16(x)
-#define  bswap32(x)  __bswap32(x)
-#define  bswap64(x)  __bswap64(x)
-
 #define SHA512_BLOCK_LENGTH    128
 #define SHA512_DIGEST_LENGTH    64
 #define SHA512_DIGEST_STRING_LENGTH  (SHA512_DIGEST_LENGTH * 2 + 1)
 
 /* Alignment-agnostic encode/decode bytestream to/from little/big endian. */
-static __inline uint16_t
-be16dec(const void *pp)
-{
-  uint8_t const *p = (uint8_t const *)pp;
-
-  return ((p[0] << 8) | p[1]);
-}
-
 static __inline uint32_t
 be32dec(const void *pp)
 {
@@ -109,39 +94,6 @@ be64dec(const void *pp)
   uint8_t const *p = (uint8_t const *)pp;
 
   return (((uint64_t)be32dec(p) << 32) | be32dec(p + 4));
-}
-
-static __inline uint16_t
-le16dec(const void *pp)
-{
-  uint8_t const *p = (uint8_t const *)pp;
-
-  return ((p[1] << 8) | p[0]);
-}
-
-static __inline uint32_t
-le32dec(const void *pp)
-{
-  uint8_t const *p = (uint8_t const *)pp;
-
-  return (((unsigned)p[3] << 24) | (p[2] << 16) | (p[1] << 8) | p[0]);
-}
-
-static __inline uint64_t
-le64dec(const void *pp)
-{
-  uint8_t const *p = (uint8_t const *)pp;
-
-  return (((uint64_t)le32dec(p + 4) << 32) | le32dec(p));
-}
-
-static __inline void
-be16enc(void *pp, uint16_t u)
-{
-  uint8_t *p = (uint8_t *)pp;
-
-  p[0] = (u >> 8) & 0xff;
-  p[1] = u & 0xff;
 }
 
 static __inline void
@@ -162,35 +114,6 @@ be64enc(void *pp, uint64_t u)
 
   be32enc(p, (uint32_t)(u >> 32));
   be32enc(p + 4, (uint32_t)(u & 0xffffffffU));
-}
-
-static __inline void
-le16enc(void *pp, uint16_t u)
-{
-  uint8_t *p = (uint8_t *)pp;
-
-  p[0] = u & 0xff;
-  p[1] = (u >> 8) & 0xff;
-}
-
-static __inline void
-le32enc(void *pp, uint32_t u)
-{
-  uint8_t *p = (uint8_t *)pp;
-
-  p[0] = u & 0xff;
-  p[1] = (u >> 8) & 0xff;
-  p[2] = (u >> 16) & 0xff;
-  p[3] = (u >> 24) & 0xff;
-}
-
-static __inline void
-le64enc(void *pp, uint64_t u)
-{
-  uint8_t *p = (uint8_t *)pp;
-
-  le32enc(p, (uint32_t)(u & 0xffffffffU));
-  le32enc(p + 4, (uint32_t)(u >> 32));
 }
 
 typedef struct SHA512Context {
