b1df4455dfd8bf99d7522fdd428402f3b6571853
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2025-11-22 12:12:21 +0800

    [server] Add currency service and tests

---
+++ core-cloud-server/Sources/CoreCloudServer/Services/CurrencyService.swift
@@ -0,0 +1,105 @@
+//
+//  CurrencyService.swift
+//  core-cloud-server
+//
+//  Created by Fang Ling on 2025/11/19.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Fluent
+import Vapor
+
+struct CurrencyService {
+  /// Adds a new currency to the database.
+  ///
+  /// - Parameters:
+  ///   - code: The unique currency code (e.g., "USD").
+  ///   - minorUnit: The number of minor units in one unit of the currency
+  ///     (e.g., 100 for cents).
+  ///   - symbol: The symbol for the currency (e.g., "$").
+  ///   - symbolPosition: The position of the currency symbol relative to the
+  ///     amount.
+  ///   - userID: The unique identifier for the user associated with the
+  ///     currency.
+  ///   - database: The database instance where the currency will be saved.
+  ///
+  /// - Returns: The unique identifier for the newly created currency.
+  ///
+  /// - Throws:
+  ///   - ``Currency.Error/databaseError`` if there is an error saving the
+  ///     currency to the database.
+  func addCurrency(
+    code: String,
+    minorUnit: Int64,
+    symbol: String,
+    symbolPosition: Currency.SymbolPosition,
+    for userID: User.IDValue,
+    on database: Database
+  ) async throws -> Currency.IDValue {
+    let currency = Currency(
+      code: code,
+      minorUnit: minorUnit,
+      symbol: symbol,
+      symbolPosition: symbolPosition.rawValue,
+      userID: userID
+    )
+
+    do {
+      try await currency.save(on: database)
+
+      return try currency.requireID()
+    } catch {
+      throw Currency.Error.databaseError
+    }
+  }
+
+  /// Retrieves a list of currencies associated with a specific user.
+  ///
+  /// - Parameters:
+  ///   - userID: The unique identifier for the user whose currencies are
+  ///     being retrieved.
+  ///   - fields: An array of field names to determine which currency
+  ///     attributes to include in the result. For example, passing
+  ///     `["code"]` will include the currency code in the results.
+  ///   - database: The database instance to query for the currencies.
+  ///
+  /// - Returns: An array of tuples, each containing:
+  ///   - `id`: The unique identifier for the currency.
+  ///   - `code`: The currency code, or `nil` if not requested in the
+  ///     `fields`.
+  ///
+  /// - Throws:
+  ///   - ``Currency.Error/databaseError`` if there is an error while
+  ///     querying the database.
+  func getCurrencies(
+    for userID: User.IDValue,
+    fields: [String],
+    on database: Database
+  ) async throws -> [(
+    id: Currency.IDValue,
+    code: String?
+  )] {
+    do {
+      return try await Currency.query(on: database)
+        .filter(\.$user.$id == userID)
+        .all()
+        .map { currency in (
+          id: try currency.requireID(),
+          code: fields.contains("code") ? currency.code : nil
+        )}
+    } catch {
+      throw Currency.Error.databaseError
+    }
+  }
+}
---
+++ core-cloud-server/Tests/CoreCloudServerTests/ServiceTests/CurrencyServiceTests.swift
@@ -0,0 +1,144 @@
+//
+//  CurrencyServiceTests.swift
+//  core-cloud-server
+//
+//  Created by Fang Ling on 2025/11/20.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+@testable import CoreCloudServer
+import Fluent
+import Testing
+import VaporTesting
+
+extension ServiceTests {
+  @Suite("CurrencyServiceTests")
+  struct CurrencyServiceTests {
+    @Test
+    func testAddCurrency() async throws {
+      let currencyService = CurrencyService()
+
+      try await withApp(configure: CoreCloudServer.configure) { app in
+        await #expect(throws: Currency.Error.databaseError) {
+          try await currencyService.addCurrency(
+            code: "USD",
+            minorUnit: 100,
+            symbol: "$",
+            symbolPosition: .leading,
+            for: 1,
+            on: app.db
+          )
+        }
+
+        let eva = User(
+          firstName: "Eva",
+          lastName: "Chan",
+          username: "eva@example.com",
+          key: Data(),
+          salt: Data(),
+          masterKeySealedBox: Data(),
+          masterKeySealedBoxSalt: Data(),
+          avatarURLs: "https://example.com/1.png"
+        )
+        try await eva.save(on: app.db)
+
+        let currencyID = try await currencyService.addCurrency(
+          code: "USD",
+          minorUnit: 100,
+          symbol: "$",
+          symbolPosition: .leading,
+          for: eva.requireID(),
+          on: app.db
+        )
+        let currency = try await Currency.query(on: app.db).first()
+        #expect(currency?.id == currencyID)
+        #expect(currency?.code == "USD")
+        #expect(currency?.minorUnit == 100)
+        #expect(currency?.symbol == "$")
+        #expect(
+          currency?.symbolPosition == Currency.SymbolPosition.leading.rawValue
+        )
+
+        await #expect(throws: Currency.Error.databaseError) {
+          try await currencyService.addCurrency(
+            code: "USD",
+            minorUnit: 100,
+            symbol: "$",
+            symbolPosition: .leading,
+            for: eva.requireID(),
+            on: app.db
+          )
+        }
+      }
+    }
+
+    @Test
+    func testGetCurrencies() async throws {
+      let currencyService = CurrencyService()
+
+      try await withApp(configure: CoreCloudServer.configure) { app in
+        var currencies = try await currencyService.getCurrencies(
+          for: 1,
+          fields: [],
+          on: app.db
+        )
+        #expect(currencies.isEmpty)
+
+        let eva = User(
+          firstName: "Eva",
+          lastName: "Chan",
+          username: "eva@example.com",
+          key: Data(),
+          salt: Data(),
+          masterKeySealedBox: Data(),
+          masterKeySealedBoxSalt: Data(),
+          avatarURLs: "https://example.com/1.png"
+        )
+        try await eva.save(on: app.db)
+
+        currencies = try await currencyService.getCurrencies(
+          for: eva.requireID(),
+          fields: [],
+          on: app.db
+        )
+        #expect(currencies.isEmpty)
+
+        let currency = try Currency(
+          code: "USD",
+          minorUnit: 100,
+          symbol: "$",
+          symbolPosition: Currency.SymbolPosition.leading.rawValue,
+          userID: eva.requireID()
+        )
+        try await currency.save(on: app.db)
+
+        currencies = try await currencyService.getCurrencies(
+          for: eva.requireID(),
+          fields: ["code"],
+          on: app.db
+        )
+        #expect(currencies.count == 1)
+        #expect(currencies.first?.id != nil)
+        #expect(currencies.first?.code == "USD")
+
+        currencies = try await currencyService.getCurrencies(
+          for: 2,
+          fields: ["code"],
+          on: app.db
+        )
+        #expect(currencies.isEmpty)
+      }
+    }
+  }
+}
