74e085da153a8cf2b864eb77c4dc9999f73dbefb
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2025-12-07 15:24:25 +0800

    [client] Initial commit

---
+++ core-cloud-client/CoreCloudClient/CoreCloudClientApp.swift
@@ -0,0 +1,30 @@
+//
+//  CoreCloudClientApp.swift
+//  CoreCloudClient
+//
+//  Created by Fang Ling on 2025/12/5.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import SwiftUI
+
+@main
+struct CoreCloudClientApp: App {
+  var body: some Scene {
+    Window("Home", id: "Home") {
+      Text("Hello, world!")
+    }
+    .windowStyle(.hiddenTitleBar)
+  }
+}
