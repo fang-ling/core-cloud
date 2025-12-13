80378f7e7c8e9bf6572e34f5cb312dae85eb16dc
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2025-12-13 23:58:08 +0800

    Change version string to 'core-cloud-6.1.1-RELEASE'

--- core-cloud-web/sources/core-cloud-web/src/views/shared-footer.tsx
+++ core-cloud-web/sources/core-cloud-web/src/views/shared-footer.tsx
@@ -30,7 +30,7 @@ export default function SharedFooter() {
       >
         <p>
           {Localizer.default().localize("Version")}
-          {" 6.1"}
+          {" 6.1.1"}
         </p>
         <p>
           {"Copyright © 2025 "}
